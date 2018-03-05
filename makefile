abf845565bf152d30d692c3660bf61ab009f67af
Brett Heithold <baheithold@crimson.ua.edu>
2018-03-05 02:30:02 -0600

    Implemented option handling
    -g for GST
    -r for AVL (default)
    takes two files on command line after options: corpus and commands

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-OBJS = string.o bst.o stack.o queue.o dll.o sll.o scanner.o trees.o
+OBJS = string.o queue.o sll.o scanner.o trees.o
 OOPTS = -Wall -Wextra -std=c99 -g -c
 LOPTS = -Wall -Wextra -std=c99 -g
 EXECUTABLES = trees
@@ -14,22 +14,13 @@ string.o:	string.c string.h
 queue.o:	queue.c queue.h sll.h
 		gcc $(OOPTS) queue.c
 
-stack.o:	stack.c stack.h dll.h
-		gcc $(OOPTS) stack.c
-
 sll.o:	sll.c sll.h
 		gcc $(OOPTS) sll.c
-
-dll.o:	dll.c dll.h
-		gcc $(OOPTS) dll.c
-
-bst.o:	bst.c bst.h queue.h
-		gcc $(OOPTS) bst.c
 
 scanner.o:	scanner.c scanner.h
 		gcc $(OOPTS) scanner.c
 
-trees.o:	trees.c bst.h
+trees.o:	trees.c
 		gcc $(OOPTS) trees.c
 
 test-all:	trees
--- trees.c
+++ trees.c
@@ -2,31 +2,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
-#include "integer.h"
-#include "real.h"
 #include "string.h"
-#include "heap.h"
 #include "scanner.h"
 
 /* options */
 int vOption = 0;    /* option -v            */
-int iOption = 1;    /* option -i (default)  */
-int rOption = 0;    /* option -r            */
-int sOption = 0;    /* option -s            */
-int IOption = 1;    /* option -I (default)  */
-int DOption = 0;    /* option -D            */
-char *filename;
+int gOption = 0;    /* option -g            */
+int rOption = 1;    /* option -r (default)  */
+
+char *corpusFilename;
+char *commandsFilename;
 
 static int processOptions(int, char **);
 void Fatal(char *,...);
-void printNameAndExplanation(void);
-void readIntegerFileToHeap(HEAP *h, FILE *fp);
-void readRealFileToHeap(HEAP *h, FILE *fp);
-void readStringFileToHeap(HEAP *h, FILE *fp);
-void displaySortedHeap(HEAP *h,
-                       void (*display)(void *, FILE *),
-                       void (*freeValue)(void *),
-                       FILE *fp);
+void printAuthor(void);
+void readStringAndTokenFile(FILE *corpusFP);
 
 int main(int argc,char **argv) {
     int argIndex;
@@ -36,74 +26,37 @@ int main(int argc,char **argv) {
     argIndex = processOptions(argc,argv);
 
     if (vOption) {
-        printNameAndExplanation();
+        printAuthor();
         return 0;
     }
 
     if (argIndex == argc) {
-        Fatal("Missing file name!\n");
+        Fatal("Missing corpus file name!\n");
+    }
+    else if (argIndex == argc - 1) {
+        Fatal("Missing commands file name!\n");
     }
 
     // Open data file for reading
-    filename = argv[argIndex];
-    FILE *fp = fopen(filename, "r");
-    if (fp == 0) Fatal("Unable to open %s for reading!\n", filename);
+    corpusFilename = argv[argIndex];
+    commandsFilename = argv[++argIndex];
+    FILE *corpusFP = fopen(corpusFilename, "r");
+    if (corpusFP == 0) {
+        Fatal("Unable to open %s for reading!\n", corpusFilename);
 
-    HEAP *h;
-    if (rOption) {
-        // Sort reals
-        if (DOption) {
-            // Sort in decreasing order
-            h = newHEAP(displayREAL, rcompareREAL, freeREAL);
-            readRealFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displayREAL, freeREAL, stdout);
-        }
-        else {
-            // Sort in increasing order
-            h = newHEAP(displayREAL, compareREAL, freeREAL);
-            readRealFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displayREAL, freeREAL, stdout);
-        }
     }
-    else if (sOption) {
-        // Sort strings
-        if (DOption) {
-            // Sort in decreasing order
-            h = newHEAP(displaySTRING, rcompareSTRING, freeSTRING);
-            readStringFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displaySTRING, freeSTRING, stdout);
-        }
-        else {
-            // Sort in increasing order
-            h = newHEAP(displaySTRING, compareSTRING, freeSTRING);
-            readStringFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displaySTRING, freeSTRING, stdout);
-        }
-    }
-    else {
-        // Sort integers
-        if (DOption) {
-            // Sort in decreasing order
-            h = newHEAP(displayINTEGER, rcompareINTEGER, freeINTEGER);
-            readIntegerFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displayINTEGER, freeINTEGER, stdout);
-        }
-        else {
-            // Sort in increasing order
-            h = newHEAP(displayINTEGER, compareINTEGER, freeINTEGER);
-            readIntegerFileToHeap(h, fp);
-            buildHEAP(h);
-            displaySortedHeap(h, displayINTEGER, freeINTEGER, stdout);
-        }
+    FILE *commandsFP = fopen(commandsFilename, "r");
+    if (commandsFP == 0) {
+        Fatal("Unable to open %s for reading!\n", commandsFilename);
     }
 
-    fclose(fp);
-    freeHEAP(h);
+    if (gOption == 1) printf("Using GST!\n");
+    else printf("Using AVL!\n");
+    printf("Reading corpus file: %s\n", corpusFilename);
+    printf("Reading commands file: %s\n", commandsFilename);
+
+    fclose(corpusFP);
+    fclose(commandsFP);
     return 0;
 }
 
@@ -142,20 +95,11 @@ static int processOptions(int argc, char **argv) {
             case 'v':
                 vOption = 1;
                 break;
-            case 'i':
-                iOption = 1;
+            case 'g':
+                gOption = 1;
                 break;
             case 'r':
                 rOption = 1;
-                break;
-            case 's':
-                sOption = 1;
-                break;
-            case 'I':
-                IOption = 1;
-                break;
-            case 'D':
-                DOption = 1;
                 break;
             default:
                 Fatal("option %s not understood\n",argv[argIndex]);
@@ -167,64 +111,17 @@ static int processOptions(int argc, char **argv) {
 }
 
 
-void printNameAndExplanation(void) {
+void printAuthor(void) {
     printf("Written by Brett Heithold\n");
-    printf("The heapsort algorithm runs in nlogn time. Inserting a value into"
-            "the heap takes constant time. Constant time insertion is accomplished"
-            "by using a queue and stack to keep track of values as they are"
-            "inserted. Every new item added into the unordered heap is inserted"
-            "at the leftmost open spot in the bottom level of the heap. Building"
-            "the heap takes linear time. Build heap accomplishes linear time"
-            "by visiting each of the leaf nodes and running heapify on their parent"
-            "nodes, then their grandparents nodes, continuing until the root"
-            "node is reached. The extract heap operation takes logn time. The"
-            "logarithmic run time of extract heap is accomplished by first"
-            "swapping the root's value with the rightmost value on the bottom"
-            "level of the heap. Then extract heap prunes the leaf node and"
-            "heapifies the root node of the tree.\n");
 }
 
 
-void readIntegerFileToHeap(HEAP *h, FILE *fp) {
-    int integer = readInt(fp);
-    while (!feof(fp)) {
-        insertHEAP(h, newINTEGER(integer));
-        integer = readInt(fp);
-    }
-}
-
-
-void readRealFileToHeap(HEAP *h, FILE *fp) {
-    double real = readReal(fp);
-    while (!feof(fp)) {
-        insertHEAP(h, newREAL(real));
-        real = readReal(fp);
-    }
-}
-
-
-void readStringFileToHeap(HEAP *h, FILE *fp) {
+void readStringAndTokenFile(FILE *fp) {
     char *str;
     if (stringPending(fp)) str = readString(fp);
     else str = readToken(fp);
     while (!feof(fp)) {
-        insertHEAP(h, newSTRING(str));
         if (stringPending(fp)) str = readString(fp);
         else str = readToken(fp);
     }
-}
-
-
-void displaySortedHeap(HEAP *h,
-                       void (*display)(void *, FILE *),
-                       void (*freeValue)(void *),
-                       FILE *fp) {
-    void *extracted;
-    while (sizeHEAP(h) > 0) {
-        extracted = extractHEAP(h);
-        display(extracted, fp);
-        freeValue(extracted);
-        if (sizeHEAP(h) > 0) fprintf(fp, " ");
-    }
-    fprintf(fp, "\n");
 }
